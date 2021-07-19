669864d73667c458e4f2d35eb31c3455eb900cc4
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-07-19 10:35:45 +0900

    Completion ThirdActivity.java

--- app/src/main/java/kr/hs/emirim/chaehyeon/project_10_1/ThirdActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/project_10_1/ThirdActivity.java
@@ -2,7 +2,10 @@ package kr.hs.emirim.chaehyeon.project_10_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        Button btnThirdClose = findViewById(R.id.btn_third_close);
+        btnThirdClose.setOnClickListener(btnThirdCloseListener);
     }
+
+    View.OnClickListener btnThirdCloseListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplication(), SecondActivity.class);
+            startActivity(intent);
+        }
+    };
 }
